3a5b1c5e8d66247f427e3b79e2cc563332296068
Rick South <rick@natamus.com>
2026-04-10 02:54:05 +0200

    Grabby Mobs source code for 26.1.2.

---
+++ Common/src/main/java/com/natamus/grabbymobs/ModCommon.java
@@ -0,0 +1,14 @@
+package com.natamus.grabbymobs;
+
+import com.natamus.grabbymobs.util.Util;
+
+public class ModCommon {
+
+	public static void init() {
+		load();
+	}
+
+	private static void load() {
+		Util.setEquipmentData();
+	}
+}
